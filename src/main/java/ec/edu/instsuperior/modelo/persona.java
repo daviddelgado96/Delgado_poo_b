b0fc13e73a7cf7ddcf5807c5951bc64cbf0e8f33
USUARIO <daviddelgadoL>
2021-08-11 20:19:09 -0500

    esta es el examen del primer bimestre

---
+++ src/main/java/ec/edu/instsuperior/modelo/persona.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.instsuperior.modelo;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class persona {
+
+    public String getMombre() {
+        return mombre;
+    }
+
+    public void setMombre(String mombre) {
+        this.mombre = mombre;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    public String getEmpeado() {
+        return empeado;
+    }
+
+    public void setEmpeado(String empeado) {
+        this.empeado = empeado;
+    }
+
+    public String getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(String sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDirectivo() {
+        return directivo;
+    }
+
+    public void setDirectivo(String directivo) {
+        this.directivo = directivo;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    private String mombre;
+    private String edad; 
+    private String empeado;
+    private String sueldo;
+    private String cliente;
+     private String telefono;
+     private String directivo;
+     private String categoria;
+     private String empresa;
+     private String nombre;}
+
+
+     
+     
+     
+
+    
+    
+     
+     
+     
+    
+
